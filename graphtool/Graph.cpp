82c1b8932ad7745e62f4435544a13c52f92e9618
089 <089@users.noreply.github.com>
2017-05-09 19:15:59 +0200

    Implemented some features (#31)
    * - implemented getter for graph type (directed or undirected)
    - resolves #26
    
    * - implemented functions for ingoing and outgoing degrees
    - resolves #27
    
    * - moved doc / comments to .h file
    
    * - implemented isFreeOfLoops
    - resolved #30
    
    * - implemented isMultigraph
    - resolved #29
    
    * - implemented isSimple (graph)
    - resolved #28
    
    * - fixed caching at isFreeOfLoops and isMultigraph function
    - implemented isComplete
    - resolves #23
    
    * - fixed caching of inDeg / outDeg
    - implemented isRegular
    - resolves #24

--- graphtool/Graph.cpp
+++ graphtool/Graph.cpp
@@ -35,3 +35,206 @@ const vector<vector<int>> &Graph::getAdjacencyMatrix() const {
 int Graph::getNumberOfNodes() const {
     return this->adjacencyMatrix.size();
 }
+
+int Graph::getInDeg(int vertexIndex) {
+
+    if (vertexIndex < 0 || vertexIndex >= this->getNumberOfNodes()) {
+        throw invalid_argument("vertex index has to be between 0 and n, but is " + to_string(vertexIndex) + ".");
+    }
+
+    // no value cached ==> calculate degree
+    if (this->inDeg.empty() || this->inDeg[vertexIndex] < 1) {
+
+        // count edges
+        int countIngoingEdges = 0;
+        for (int row = 0; row < this->getNumberOfNodes(); row++) {
+            // adds ingoing edges
+            countIngoingEdges += this->adjacencyMatrix[row][vertexIndex];
+
+            // In undirected graphs loops are counted twice:
+            if (row == vertexIndex && !this->isDirected()) {
+                countIngoingEdges++;
+            }
+        }
+
+        // setting cache value
+        this->inDeg.resize(this->getNumberOfNodes());
+        this->inDeg[vertexIndex] = countIngoingEdges;
+    }
+
+    return this->inDeg[vertexIndex];
+}
+
+int Graph::getOutDeg(int vertexIndex) {
+
+    if (vertexIndex < 0 || vertexIndex >= this->getNumberOfNodes()) {
+        throw invalid_argument("vertex index has to be between 0 and n, but is " + to_string(vertexIndex) + ".");
+    }
+
+    // no value cached ==> calculate degree
+    if (this->outDeg.empty() || this->outDeg[vertexIndex] < 1) {
+
+        // count edges
+        int countOutgoingEdges = 0;
+        for (int col = 0; col < this->getNumberOfNodes(); col++) {
+            // adds ingoing edges
+            countOutgoingEdges += this->adjacencyMatrix[vertexIndex][col];
+
+            // In undirected graphs loops are counted twice:
+            if (col == vertexIndex && !this->isDirected()) {
+                countOutgoingEdges++;
+            }
+        }
+
+        // setting cache value
+        this->outDeg.resize(this->getNumberOfNodes());
+        this->outDeg[vertexIndex] = countOutgoingEdges;
+    }
+
+    return this->outDeg[vertexIndex];
+}
+
+bool Graph::isDirected() {
+
+    // Get cached result.
+    switch (this->type) {
+        case DIRECTED:
+            return true;
+
+        case UNDIRECTED:
+            return false;
+
+        case UNCHECKED:
+            // check if values of the adjacency matrix are set symmetrically ==> undirected graph
+            // otherwise diagraph
+            for (int row = 0; row < this->getNumberOfNodes(); row++) {
+                // check only upper triangle part of matrix
+                for (int col = row + 1; col < this->getNumberOfNodes(); col++) {
+                    // values must be the same, otherwise its a digraph
+                    if (this->getAdjacencyMatrix()[row][col] != this->getAdjacencyMatrix()[col][row]) {
+                        // cache result
+                        this->type = DIRECTED;
+                        return true;
+                    }
+                }
+            }
+
+            break;
+    }
+
+    // cache result
+    this->type = UNDIRECTED;
+    return false;
+}
+
+bool Graph::isFreeOfLoops() {
+
+    if (isFreeOfLoopsFlag) {
+        return isFreeOfLoopsCache;
+    }
+
+    isFreeOfLoopsFlag = true;
+
+    for (int mainDiagonal = 0; mainDiagonal < this->getNumberOfNodes(); ++mainDiagonal) {
+        // check if there is a loop
+        if (this->getAdjacencyMatrix()[mainDiagonal][mainDiagonal] > 0) {
+            isFreeOfLoopsCache = false;
+            return isFreeOfLoopsCache;
+        }
+    }
+
+    isFreeOfLoopsCache = true;
+    return isFreeOfLoopsCache;
+}
+
+bool Graph::isMultigraph() {
+
+    if (isMultigraphFlag) {
+        return isMultigraphCache;
+    }
+
+    isMultigraphFlag = true;
+
+    /*
+     * if the adjacency matrix contains a value greater than 1 the matrix
+     * has multiple edges and therefore the graph is a multigraph.
+     */
+    for (int row = 0; row < this->getNumberOfNodes(); ++row) {
+        for (int col = 0; col < this->getNumberOfNodes(); ++col) {
+            if (this->getAdjacencyMatrix()[row][col] > 1) {
+                isMultigraphCache = true;
+                return isMultigraphCache;
+            }
+        }
+    }
+
+    isMultigraphCache = false;
+    return isMultigraphCache;
+}
+
+bool Graph::isSimple() {
+    return !this->isDirected() && !isMultigraph() && isFreeOfLoops();
+}
+
+bool Graph::isComplete() {
+
+    if (isCompleteFlag) {
+        return isCompleteCache;
+    }
+
+    isCompleteFlag = true;
+
+
+    /*
+     * the graph is complete if the adjacency matrix has only entries of 1
+     * except the main diagonal which must be 0.
+     */
+    if (!isFreeOfLoops()) {
+        isCompleteCache = false;
+        return isCompleteCache;
+    }
+
+    for (int row = 0; row < this->getNumberOfNodes(); row++) {
+        for (int col = 0; col < this->getNumberOfNodes(); col++) {
+            // only 1 is allowed now
+            if (row != col && this->getAdjacencyMatrix()[row][col] != 1) {
+                isCompleteCache = false;
+                return isCompleteCache;
+            }
+        }
+    }
+
+    isCompleteCache = true;
+    return isCompleteCache;
+}
+
+bool Graph::isRegular() {
+
+    if (isRegularFlag) {
+        return isRegularCache;
+    }
+
+    isRegularFlag = true;
+
+    // get in/out deg of the first vertex
+    int inDegOfFirstVertex = this->getInDeg(0);
+    int outDegOfFirstVertex = this->getOutDeg(0);
+
+    // all other vertices must have the same deg,
+    // otherwise this graph is not regular
+    for (int vertex = 1; vertex < this->getNumberOfNodes(); vertex++) {
+
+
+        // directed graph ==> indeg and outdeg can differ ==> we have to test both
+        // undirected graph ==> indeg and outdeg must be the same ==> we only must test one
+        // ==> we can use the following commands for both types
+        if (this->getInDeg(vertex) != inDegOfFirstVertex || this->getOutDeg(vertex) != outDegOfFirstVertex) {
+            isRegularCache = false;
+            return isRegularCache;
+        }
+
+    }
+
+    isRegularCache = true;
+    return isRegularCache;
+}
--- graphtool/Graph.h
+++ graphtool/Graph.h
@@ -13,8 +13,70 @@ class Graph {
 private:
     /**
      * the adjacency matrix
-     */   
+     */
     vector<vector<int>> adjacencyMatrix;
+
+    /**
+     * Possible types of the graph: unchecked, directed or undirected.
+     */
+    enum Type {
+        UNCHECKED, DIRECTED, UNDIRECTED
+    };
+
+    /**
+     * type of the graph ({@see Type).
+     */
+    Type type = UNCHECKED;
+
+    /**
+     * Contains the <b>ingoing</b> degrees of the individual vertices.
+     */
+    vector<int> inDeg;
+
+    /**
+     * Contains the <b>outgoing</b> degrees of the individual vertices.
+     */
+    vector<int> outDeg;
+
+    /**
+     * Cache for result of eponymous function.
+     */
+    bool isFreeOfLoopsCache = false;
+
+    /**
+     * Flag. Specifies whether the eponymous function has been called already.
+     */
+    bool isFreeOfLoopsFlag = false;
+
+    /**
+     * Cache for result of eponymous function.
+     */
+    bool isMultigraphCache = false;
+
+    /**
+     * Flag. Specifies whether the eponymous function has been called already.
+     */
+    bool isMultigraphFlag = false;
+
+    /**
+     * Cache for result of eponymous function.
+     */
+    bool isCompleteCache = false;
+
+    /**
+     * Flag. Specifies whether the eponymous function has been called already.
+     */
+    bool isCompleteFlag = false;
+
+    /**
+     * Cache for result of eponymous function.
+     */
+    bool isRegularCache = false;
+
+    /**
+     * Flag. Specifies whether the eponymous function has been called already.
+     */
+    bool isRegularFlag = false;
 
 public:
     Graph(vector<vector<int>> adjacencyMatrix);
@@ -28,6 +90,77 @@ public:
     int getNumberOfNodes() const;
 
     const vector<vector<int>> &getAdjacencyMatrix() const;
+
+    /**
+     * Returns if the graph is a directed or undirected graph.
+     *
+     * @return  true, if directed<br>
+     *          false, if undirected
+     */
+    bool isDirected();
+
+    /**
+     * Returns indegree indeg(v). Therefore it counts the ingoing edges.
+     * In undirected graphs loops are counted twice.
+     *
+     * @param vertexIndex   the index of the vertex v
+     * @return indegree indeg(v)
+     */
+    int getInDeg(int vertexIndex);
+
+    /**
+     * Returns outdegree outdeg(v). Therefore it counts the outgoing edges.
+     * In undirected graphs loops are counted twice.
+     *
+     * @param vertexIndex   the index of the vertex v
+     * @return outdegree outdeg(v)
+     */
+    int getOutDeg(int vertexIndex);
+
+    /**
+     * Specifies whether the graph is free of loops.
+     * Loops are edges from and to the same vertex.
+     *
+     * @return true, if there are no loops in the graph.
+     */
+    bool isFreeOfLoops();
+
+    /**
+     * Specifies whether the graph is a multigraph (has multiple edges/has
+     * <a href="https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)#Multigraph">
+     * "two or more edged that connect the same two vertices"</a>)
+     *
+     * @return true, if it is a multigraph
+     */
+    bool isMultigraph();
+
+    /**
+     * Specifies whether the graph is a simple graph (undirected, no multiple
+     * edges, no loops) or not.
+     *
+     * @return  true, if the graph is a simple graph.
+     */
+    bool isSimple();
+
+    /**
+     * Specifies whether the graph is a complete graph (simple graph
+     * <a href="https://en.wikipedia.org/wiki/Complete_graph">"in which every
+     * pair of distinct vertices is connected by a unique edge."</a>).
+     * Checks only undirected graphs! A complete graph is also called K_n.
+     *
+     * @return  true, if the graph is a complete graph.
+     */
+    bool isComplete();
+
+    /**
+     * Specifies whether the graph is a regular graph
+     * (<a href="https://en.wikipedia.org/wiki/Regular_graph">
+     * "is a graph where each vertex has the same number of neighbors"
+     * </a>).
+     *
+     * @return  true, if the graph is a regular graph.
+     */
+    bool isRegular();
 };
 
 
