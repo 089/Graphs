896861ceff86c35754455575a9ec7d4b4c56deed
Stieglitz <kevin@stieglitz-online.de>
2017-05-11 13:58:13 +0200

    Feature/doxygen ... again.  (#35)
    * Update .travis.yml
    
    Test doxygen
    
    * Update doxygen generation
    
    * Update Doxygen config
    
    * Fix doxygen filepath

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -39,65 +39,69 @@ echo 'Setting up the script...'
 # Exit with nonzero exit code if anything fails
 set -e
 
-# Create a clean working directory for this script.
-mkdir code_docs
-cd code_docs
+if [ "$GENERATE_DOC" = true ] ; then
 
-# Get the current gh-pages branch
-git clone -b gh-pages https://git@$GH_REPO_REF
-cd $GH_REPO_NAME
+  GH_REPO_ORG=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 1`
+  GH_REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 2`
+  GH_REPO_REF="github.com/$GH_REPO_ORG/$GH_REPO_NAME.git"
 
-##### Configure git.
-# Set the push default to simple i.e. push only the current branch.
-git config --global push.default simple
-# Pretend to be an user called Travis CI.
-git config user.name "Travis CI"
-git config user.email "travis@travis-ci.org"
+  git clone -b gh-pages https://git@$GH_REPO_REF code_docs
+  cd code_docs
 
-# Remove everything currently in the gh-pages branch.
-# GitHub is smart enough to know which files have changed and which files have
-# stayed the same and will only update the changed files. So the gh-pages branch
-# can be safely cleaned, and it is sure that everything pushed later is the new
-# documentation.
-rm -rf *
+  ##### Configure git.
+  # Set the push default to simple i.e. push only the current branch.
+  git config --global push.default simple
+  # Pretend to be an user called Travis CI.
+  git config user.name "Travis CI"
+  git config user.email "travis@travis-ci.org"
 
-# Need to create a .nojekyll file to allow filenames starting with an underscore
-# to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
-# Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
-# to NO, which it is by default. So creating the file just in case.
-echo "" > .nojekyll
+  # Remove everything currently in the gh-pages branch.
+  # GitHub is smart enough to know which files have changed and which files have
+  # stayed the same and will only update the changed files. So the gh-pages branch
+  # can be safely cleaned, and it is sure that everything pushed later is the new
+  # documentation.
+  rm -rf *
 
-################################################################################
-##### Generate the Doxygen code documentation and log the output.          #####
-echo 'Generating Doxygen code documentation...'
-# Redirect both stderr and stdout to the log file AND the console.
-doxygen $DOXYFILE 2>&1 | tee doxygen.log
+  # Need to create a .nojekyll file to allow filenames starting with an underscore
+  # to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
+  # Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
+  # to NO, which it is by default. So creating the file just in case.
+  echo "" > .nojekyll
 
-################################################################################
-##### Upload the documentation to the gh-pages branch of the repository.   #####
-# Only upload if Doxygen successfully created the documentation.
-# Check this by verifying that the html directory and the file html/index.html
-# both exist. This is a good indication that Doxygen did it's work.
-if [ -d "html" ] && [ -f "html/index.html" ]; then
+  ################################################################################
+  ##### Generate the Doxygen code documentation and log the output.          #####
+  echo 'Generating Doxygen code documentation...'
+  # Redirect both stderr and stdout to the log file AND the console.
+  doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
-    echo 'Uploading documentation to the gh-pages branch...'
-    # Add everything in this directory (the Doxygen code documentation) to the
-    # gh-pages branch.
-    # GitHub is smart enough to know which files have changed and which files have
-    # stayed the same and will only update the changed files.
-    git add --all
+  ################################################################################
+  ##### Upload the documentation to the gh-pages branch of the repository.   #####
+  # Only upload if Doxygen successfully created the documentation.
+  # Check this by verifying that the html directory and the file html/index.html
+  # both exist. This is a good indication that Doxygen did it's work.
+  if [ -d "doxygen_doc/html" ] && [ -f "doxygen_doc/html/index.html" ]; then
 
-    # Commit the added files with a title and description containing the Travis CI
-    # build number and the GitHub commit reference that issued this build.
-    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+      echo 'Uploading documentation to the gh-pages branch...'
+      # Add everything in this directory (the Doxygen code documentation) to the
+      # gh-pages branch.
+      # GitHub is smart enough to know which files have changed and which files have
+      # stayed the same and will only update the changed files.
+      git add --all
 
-    # Force push to the remote gh-pages branch.
-    # The ouput is redirected to /dev/null to hide any sensitive credential data
-    # that might otherwise be exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+      # Commit the added files with a title and description containing the Travis CI
+      # build number and the GitHub commit reference that issued this build.
+      git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+
+      # Force push to the remote gh-pages branch.
+      # The ouput is redirected to /dev/null to hide any sensitive credential data
+      # that might otherwise be exposed.
+      git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+  else
+      echo '' >&2
+      echo 'Warning: No documentation (html) files have been found!' >&2
+      echo 'Warning: Not going to push the documentation to GitHub!' >&2
+      exit 1
+  fi
 else
-    echo '' >&2
-    echo 'Warning: No documentation (html) files have been found!' >&2
-    echo 'Warning: Not going to push the documentation to GitHub!' >&2
-    exit 1
+  echo 'GENERATE_DOC not set' >&2
 fi
