4814cbe4517df1f213a2e3252b754e0d1ae18520
089 <089@users.noreply.github.com>
2017-05-28 12:08:54 +0200

    mze issue38 (#39)
    * - implemented areNeighbours, resolves #38
    - added unit tests
    
    * - implemented areNeighbours, resolves #38
    - added unit tests

--- test/main.cpp
+++ test/main.cpp
@@ -178,6 +178,145 @@ TEST(graph_check, test_free_of_loops_2) {
     EXPECT_FALSE(g->isFreeOfLoops());
 }
 
+TEST(graph_check, test_are_neighbours_1) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 0},
+            {1, 1, 1, 0},
+            {1, 1, 0, 0},
+            {0, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+    EXPECT_FALSE(g->areNeighbours(0, 0));
+    EXPECT_FALSE(g->areNeighbours(0, 3));
+    EXPECT_FALSE(g->areNeighbours(1, 3));
+    EXPECT_FALSE(g->areNeighbours(2, 2));
+    EXPECT_FALSE(g->areNeighbours(2, 3));
+    EXPECT_FALSE(g->areNeighbours(3, 0));
+    EXPECT_FALSE(g->areNeighbours(3, 1));
+    EXPECT_FALSE(g->areNeighbours(3, 2));
+    EXPECT_FALSE(g->areNeighbours(3, 3));
+
+    EXPECT_TRUE(g->areNeighbours(0, 1));
+    EXPECT_TRUE(g->areNeighbours(0, 2));
+    EXPECT_TRUE(g->areNeighbours(1, 0));
+    EXPECT_TRUE(g->areNeighbours(1, 1));
+    EXPECT_TRUE(g->areNeighbours(1, 2));
+    EXPECT_TRUE(g->areNeighbours(2, 0));
+    EXPECT_TRUE(g->areNeighbours(2, 1));
+}
+
+TEST(graph_check, test_are_neighbours_directed) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+    EXPECT_FALSE(g->areNeighbours(0, 0));
+    EXPECT_FALSE(g->areNeighbours(1, 0));
+    EXPECT_FALSE(g->areNeighbours(2, 0));
+    EXPECT_FALSE(g->areNeighbours(3, 0));
+
+    EXPECT_TRUE(g->areNeighbours(0, 1));
+    EXPECT_TRUE(g->areNeighbours(0, 2));
+    EXPECT_TRUE(g->areNeighbours(0, 3));
+}
+
+TEST(graph_check, test_are_neighbours_undirected) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+    EXPECT_TRUE(g->areNeighbours(1, 0));
+    EXPECT_TRUE(g->areNeighbours(2, 0));
+    EXPECT_TRUE(g->areNeighbours(3, 0));
+
+    EXPECT_TRUE(g->areNeighbours(0, 1));
+    EXPECT_TRUE(g->areNeighbours(0, 2));
+    EXPECT_TRUE(g->areNeighbours(0, 3));
+}
+
+TEST(graph_check, test_are_neighbours_range_1) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+
+    // throw out_of_range("from/to must be in [0, " + getNumberOfNodes() + string("[."));
+    try {
+        g->areNeighbours(-1,3);
+        FAIL();
+    } catch (const out_of_range& e) {
+        cout << e.what() << endl;
+        ASSERT_STREQ("from and to have to be in the range. ", e.what());
+    }
+}
+
+TEST(graph_check, test_are_neighbours_range_2) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+
+    // throw out_of_range("from/to must be in [0, " + getNumberOfNodes() + string("[."));
+    try {
+        g->areNeighbours(2,-2);
+        FAIL();
+    } catch (const out_of_range& e) {
+        cout << e.what() << endl;
+        ASSERT_STREQ("from and to have to be in the range. ", e.what());
+    }
+}
+
+TEST(graph_check, test_are_neighbours_range_3) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+
+    // throw out_of_range("from/to must be in [0, " + getNumberOfNodes() + string("[."));
+    try {
+        g->areNeighbours(0,4);
+        FAIL();
+    } catch (const out_of_range& e) {
+        cout << e.what() << endl;
+        ASSERT_STREQ("from and to have to be in the range. ", e.what());
+    }
+}
+
+TEST(graph_check, test_are_neighbours_range_4) {
+    vector<vector<int>> am = {
+            {0, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+    };
+    Graph *g = new Graph(am);
+
+    // throw out_of_range("from/to must be in [0, " + getNumberOfNodes() + string("[."));
+    try {
+        g->areNeighbours(4,0);
+        FAIL();
+    } catch (const out_of_range& e) {
+        cout << e.what() << endl;
+        ASSERT_STREQ("from and to have to be in the range. ", e.what());
+    }
+}
+
+
 TEST(graph_check, test_matlab_correct_notation) {
 
     try {
