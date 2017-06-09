782e7832ac541d300c6a5f0e0b5601895d9cd060
Travis CI <travis@travis-ci.org>
2017-06-09 17:52:02 +0000

    Deploy code docs to GitHub Pages Travis build: 118
    Commit: c0ed56667c214d35cffa89f6ba26b2fa8ccadcd5

--- doxygen_doc/html/search/functions_5.js
+++ doxygen_doc/html/search/functions_5.js
@@ -5,6 +5,5 @@ var searchData=
   ['isfreeofloops',['isFreeOfLoops',['../class_graph.html#a451bbc2d9ddde5e85d6c122a845dbd02',1,'Graph']]],
   ['ismultigraph',['isMultigraph',['../class_graph.html#ab410dec3635c080f04c60c6835d91df6',1,'Graph']]],
   ['isregular',['isRegular',['../class_graph.html#afe19c30e6b7005fd0c66c5f182764ca5',1,'Graph']]],
-  ['issimple',['isSimple',['../class_graph.html#a4095f5949d5b2a420ecf2d0f2bb68dc8',1,'Graph']]],
-  ['issquarematrix',['isSquareMatrix',['../class_graph.html#a00f613f4d274a895e08988074a3a9e9a',1,'Graph']]]
+  ['issimple',['isSimple',['../class_graph.html#a4095f5949d5b2a420ecf2d0f2bb68dc8',1,'Graph']]]
 ];
